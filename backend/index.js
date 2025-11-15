63328999070305140d84e7d2ccc3e052c350df80
Kailash Singh <kailashsingh9711@gmail.com>
2025-11-15 05:59:23 +0530

    added all models

--- backend/index.js
+++ backend/index.js
@@ -39,3 +39,7 @@ app.listen(port,()=>{
     connectDB(),
     console.log(`server is running ${port}`)
 })
+
+//git remote add origin https://github.com/kailashsingh9711-cpu/job-portal.git
+//git branch -M main
+//git push -u origin main
--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
 const jobSchema = new mongoose.Schema({
-    clearImmediate
-})
+    title:{type:String,required:true},
+    discreption:{type:String,required:true},
+    requirement:[{type:String}],
+    salary:{type:Number,required:true},
+    location:{type:String,required:true},
+    jobType:{enum:['part-time','full-time','itern'],required:true},
+    position:{type:Number,required:true},
+    company:{type:mongoose.Schema.Types.ObjectId,ref:'Company',required:true},
+    createdAt:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+    applications:[{type:mongoose.Schema.Types.ObjectId,ref:'Application'}]
+},{timestamps:true})
+
+export const Job = mongoose.model('Job',jobSchema);
